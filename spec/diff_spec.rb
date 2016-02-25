f2d20dc8f878e5f7865e6115311d8355269cb3f0
Jacob Vosmaer <contact@jacobvosmaer.nl>
2016-02-25 16:05:44 +0100

    Use "its"

--- spec/diff_spec.rb
+++ spec/diff_spec.rb
@@ -53,7 +53,7 @@ EOT
     subject { diffs }
 
     it { should be_kind_of Gitlab::Git::DiffCollection }
-    it { subject.count.should eq(1) }
+    its(:count) { should eq(1) }
 
     context :diff do
       subject { diffs.first }
