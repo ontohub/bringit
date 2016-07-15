703105c381b873da5b35f8a3a13fba237c613511
Paco Guzman <pacoguzmanp@gmail.com>
2016-07-15 12:14:24 +0200

    Handle collapsable DiffCollection
    We want to give the user the opportunity to see more
    files while preserving safe limits on rendering diffs
    so files bigger than 10KB (we consider too large 100KB)
    their lines are not taking into account to decide
    if the collection overflow and you can decide if
    you want to expand them on click or no.
    
    To keep good performance we start collapsing all
    the files since we are over the safe limits.
    
    To do that you have to methods `collapsed?` and
    `collapse` which means if it's quite large and the
    other if the collection collapsed it when iterated
    as a whole.
    
    It's quite similar to the `too_large`concept but
    give us a little bit more flexibility.

--- lib/gitlab_git/diff_collection.rb
+++ lib/gitlab_git/diff_collection.rb
@@ -10,7 +10,11 @@ module Gitlab
         @max_files = options.fetch(:max_files, DEFAULT_LIMITS[:max_files])
         @max_lines = options.fetch(:max_lines, DEFAULT_LIMITS[:max_lines])
         @max_bytes = @max_files * 5120 # Average 5 KB per file
+        @safe_max_files = [@max_files, DEFAULT_LIMITS[:max_files]].min
+        @safe_max_lines = [@max_lines, DEFAULT_LIMITS[:max_lines]].min
+        @safe_max_bytes = @safe_max_files * 5120 # Average 5 KB per file
         @all_diffs = !!options.fetch(:all_diffs, false)
+        @no_collapse = !!options.fetch(:no_collapse, true)
 
         @line_count = 0
         @byte_count = 0
@@ -45,6 +49,12 @@ module Gitlab
           # doesn't add up to the total amount of lines.
           if diff.too_large?
             diff.prune_large_diff!
+          end
+
+          if !@all_diffs && !@no_collapse
+            if diff.collapsible? || over_safe_limits?(i)
+              diff.prune_collapsed_diff!
+            end
           end
 
           @line_count += diff.line_count
@@ -98,6 +108,10 @@ module Gitlab
         each { nil } # force a loop through all diffs
         @populated = true
         nil
+      end
+
+      def over_safe_limits?(files)
+        files >= @safe_max_files || @line_count > @safe_max_lines || @byte_count >= @safe_max_bytes
       end
     end
   end
