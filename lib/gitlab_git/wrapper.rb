1539c0ff9fe033d8c05be10d8bd14e27b9ab106b
Eugen Kuksa <kuksa.eugen@gmail.com>
2017-07-07 12:42:36 +0200

    Expose diff method to the wrapper.

--- lib/gitlab_git/wrapper.rb
+++ lib/gitlab_git/wrapper.rb
@@ -12,8 +12,8 @@ module Gitlab
 
       attr_reader :gitlab
       delegate :bare?, :branches, :branch_count, :branch_exists?, :branch_names,
-               :commit_count, :find_commits, :empty?, :log, :ls_files, :rugged,
-               to: :gitlab
+               :commit_count, :diff, :find_commits, :empty?, :log, :ls_files,
+               :rugged, to: :gitlab
 
       def self.create(path)
         raise Error, "Path #{path} already exists." if Pathname.new(path).exist?
